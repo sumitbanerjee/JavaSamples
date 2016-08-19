c1148527cf76920958eeab7c01b1b93b578dce7b
sumitbanerjee <include.sumit@gmail.com>
2016-08-19 10:21:52 -0400

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,12 +1,11 @@
 package com.sitepal.tests;
 
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
+import org.w3c.css.sac.ElementSelector;
 
 public class Test1 {
 
@@ -18,29 +17,43 @@ public class Test1 {
 		WebDriver driver = new ChromeDriver();
 		
 		//OPen Sitepal home page
-		driver.get("http://www.sitepal.com");
-		Thread.sleep(2000);
-		driver.findElement(By.linkText("Login")).click();
-		System.out.println("Welcome Oddcast");
-		Thread.sleep(2000);
-		System.out.println("send username");
-		Thread.sleep(6000);
-		try
-		{
-		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("sbanerjee@oddcast.com");
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("sumit1140");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@class='btn_blue_125']")).click();
-		driver.findElement(By.xpath("//*[@id='btn_blue_125']")).click();
-		
-		System.out.println("Successfully logged in VHSS");
-		}
-		catch(Exception e)
-		{
-			System.out.println("element not foud");
-		}
-	}
-
+				driver.get("http://www.sitepal.com");
+				Thread.sleep(2000);
+				
+				driver.findElement(By.linkText("Login")).click();
+				System.out.println("Welcome Oddcast");
+				Thread.sleep(2000);
+				System.out.println("send username");
+				Thread.sleep(6000);
+				try
+				{
+				driver.findElement(By.xpath("//input[@name='email']")).sendKeys("sbanerjee@oddcast.com");
+				driver.findElement(By.xpath("//input[@name='password']")).sendKeys("sumit1140");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath("//input[@type='submit']")).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath("//input[@class='btn_blue_125']")).click();
+				Thread.sleep(2000);
+				driver.findElement(By.id("door")).click();
+				Select drop = new Select(driver.findElement(By.id("door")));
+				drop.selectByIndex(17);
+				Thread.sleep(2000);
+				driver.findElement(By.id("oneClick14")).click();
+				Thread.sleep(2000);
+				Select DropDown = new Select(driver.findElement(By.id("searchby")));
+				DropDown.selectByIndex(3);
+				Thread.sleep(2000);
+				driver.findElement(By.cssSelector("input[name^=’keyword’]")).sendKeys("sss");
+				
+				
+			
+				//driver.findElement(By.xpath("//*[@id='btn_blue']")).click();
+				Thread.sleep(2000);
+				System.out.println("Successfully logged in VHSS");
+				}
+				catch(Exception e)
+				{
+					System.out.println("element not foud");
+				}
+			}
 }
